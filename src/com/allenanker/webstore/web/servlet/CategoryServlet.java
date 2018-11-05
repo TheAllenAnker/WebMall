056fc9e2b4cdec4c53cdb75d0d46b38857b9347d
TheAllenAnker <theallenanker@gmail.com>
2018-11-05 22:34:56 +0800

    Solve the issue: missing categories navigation in pages except index page. Using Ajax to request data from CategoryServlet.

--- src/com/allenanker/webstore/web/servlet/CategoryServlet.java
+++ src/com/allenanker/webstore/web/servlet/CategoryServlet.java
@@ -1,12 +1,26 @@
 package com.allenanker.webstore.web.servlet;
 
+import com.allenanker.webstore.domain.Category;
+import com.allenanker.webstore.service.CategoryService;
+import com.allenanker.webstore.service.serviceImp.CategoryServiceImp;
 import com.allenanker.webstore.web.base.BaseServlet;
+import net.sf.json.JSONArray;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public class CategoryServlet extends BaseServlet {
-    public String findAllCats(HttpServletRequest request, HttpServletResponse response) {
+    public String findAllCats(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+        CategoryService categoryService = new CategoryServiceImp();
+        List<Category> categoryList = categoryService.getAllCategories();
+        // list to json format
+        String jsonStr = JSONArray.fromObject(categoryList).toString();
+        response.setContentType("application/json;charset=utf-8");
+        System.out.println(jsonStr);
+        response.getWriter().write(jsonStr);
         return null;
     }
 }
--- src/com/allenanker/webstore/web/servlet/IndexServlet.java
+++ src/com/allenanker/webstore/web/servlet/IndexServlet.java
@@ -1,20 +1,13 @@
 package com.allenanker.webstore.web.servlet;
 
-import com.allenanker.webstore.domain.Category;
-import com.allenanker.webstore.service.CategoryService;
-import com.allenanker.webstore.service.serviceImp.CategoryServiceImp;
 import com.allenanker.webstore.web.base.BaseServlet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class IndexServlet extends BaseServlet {
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        CategoryService categoryService = new CategoryServiceImp();
-        List<Category> categoryList = categoryService.getAllCategories();
-        request.setAttribute("allCats", categoryList);
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
         return "/jsp/index.jsp";
     }
 }
