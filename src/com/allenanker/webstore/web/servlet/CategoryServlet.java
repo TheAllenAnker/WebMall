f30a89af4f09e3c49141b876676530c5f1911e85
TheAllenAnker <theallenanker@gmail.com>
2018-11-05 23:13:17 +0800

    Adding redis to cache the requested categories information. Speed up the server for database query.

--- src/com/allenanker/webstore/web/servlet/CategoryServlet.java
+++ src/com/allenanker/webstore/web/servlet/CategoryServlet.java
@@ -3,8 +3,10 @@ package com.allenanker.webstore.web.servlet;
 import com.allenanker.webstore.domain.Category;
 import com.allenanker.webstore.service.CategoryService;
 import com.allenanker.webstore.service.serviceImp.CategoryServiceImp;
+import com.allenanker.webstore.utils.JedisUtils;
 import com.allenanker.webstore.web.base.BaseServlet;
 import net.sf.json.JSONArray;
+import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,13 +16,21 @@ import java.util.List;
 
 public class CategoryServlet extends BaseServlet {
     public String findAllCats(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
-        CategoryService categoryService = new CategoryServiceImp();
-        List<Category> categoryList = categoryService.getAllCategories();
-        // list to json format
-        String jsonStr = JSONArray.fromObject(categoryList).toString();
+        // try to get from redis first
+        Jedis jedis = JedisUtils.getJedis();
+        String jsonStr = jedis.get("allCats");
+        System.out.println("From redis: " + jsonStr);
+        if (jsonStr == null || jsonStr.equals("")) {
+            CategoryService categoryService = new CategoryServiceImp();
+            List<Category> categoryList = categoryService.getAllCategories();
+            // list to json format
+            jsonStr = JSONArray.fromObject(categoryList).toString();
+            jedis.set("allCats", jsonStr);
+        }
         response.setContentType("application/json;charset=utf-8");
-        System.out.println(jsonStr);
         response.getWriter().write(jsonStr);
+        JedisUtils.closeJedis(jedis);
+
         return null;
     }
 }
